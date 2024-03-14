d66cfc6f472fe9c707ee0e279aaa475ec01713a2
Moses-93 <moieseenko2001@gmail.com>
2024-03-14 14:37:48 +0200

    task_2

--- task-2/task_2.py
+++ task-2/task_2.py
@@ -1,11 +1,12 @@
+def get_cats_info(path):
+    with open(path, "r",) as file_cats:
+        content = file_cats.readlines()
+    for cat in content:
+        id_, name,age = cat.split(",")
+            cats.append({"id": id_, "name": name, "age": age.strip()}) 
+    return cats
 
 file_path = r"F:\PythonCourse\Repositories\homework_m4-1\task-2\cats.txt"
 
-with open(file_path, "r",) as file_cats:
-    content = file_cats.readlines()
-    for cat in content:
-        id_, name,age = cat.split(",")
-        cats = [{"id": id_, "name": name, "age": age.strip()}]
-        
     
 
