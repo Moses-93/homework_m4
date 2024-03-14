fe079febf9984de06ec873602eb98126201655e0
Moses-93 <moieseenko2001@gmail.com>
2024-03-14 14:39:20 +0200

    task_2_add_except

--- task-2/task_2.py
+++ task-2/task_2.py
@@ -1,12 +1,19 @@
-def get_cats_info(path):
-    with open(path, "r",) as file_cats:
-        content = file_cats.readlines()
-    for cat in content:
-        id_, name,age = cat.split(",")
-            cats.append({"id": id_, "name": name, "age": age.strip()}) 
-    return cats
+def get_cats_info(path): # ф-кція, яка приймає шлях
+    try:
+        with open(path, "r",) as file_cats: # відкриваємо файл
+            cats = []
+            content = file_cats.readlines() #читаємо файл по рядках 
+            for cat in content: #ітеруємося по кожному рядку 
+                id_, name,age = cat.split(",") #розбиваємо рядок по комах 
+                cats.append({"id": id_, "name": name, "age": age.strip()}) #додаємо в список словники з данними 
+    except Exception:# оброблюємо вийнятки 
+        print("Програма не може розпізнати ваші данні")
 
-file_path = r"F:\PythonCourse\Repositories\homework_m4-1\task-2\cats.txt"
+    return cats #повертаємо список зі словниками 
 
+ 
+file_path = r"" # абсолютний шлях до файлу
+
+print(get_cats_info(file_path))
     
 
