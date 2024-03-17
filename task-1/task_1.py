d216d7d99be3e169accd56b7c83d1c5ee37a6a9a
Moses-93 <moieseenko2001@gmail.com>
2024-03-17 15:02:40 +0200

    Revert "task_1_add_try_except"
    This reverts commit 50a567ccc5f9ac50628bf3703777179d81a35871.

--- task-1/task_1.py
+++ task-1/task_1.py
@@ -1,15 +1,13 @@
 def total_salary(path): # функція яка приймає шлях до файлу 
-    try: 
-        salaries = []  # список для зберігання зарплат
-        with open(path, "r", encoding="utf-8") as file: # відкриваємо та читаємо файл за допомогою менеджера контексту  
-            for read in file.readlines(): # Перебирааємо файл з зп та зберігаємо данні
-                _, salary = read.split(",") #розділяємо по ","
-                salaries.append(int(salary))  # додаємо зарплату до списку
+    salaries = []  # список для зберігання зарплат
+    with open(path, "r", encoding="utf-8") as file: # відкриваємо та читаємо файл за допомогою менеджера контексту  
+        for read in file.readlines():
+            _, salary = read.split(",")
+            salaries.append(int(salary))  # додаємо зарплату до списку
 
-        full = sum(salaries)  # сумуємо ЗП 
-        medium = full / len(salaries)  # ділимо на кількість робітників 
-    except ValueError as error:
-        print(error)
+    full = sum(salaries)  # сумуємо ЗП 
+    medium = full / len(salaries)  # ділимо на кількість робітників 
+
     return "Загальна сумма:", full, "Cередня сумма:", medium # повертаємо загальну та середню зп 
 
 file_path = r"" # абсолютний шлях до файлу
