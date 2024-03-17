cadb06b1b4cf828067ff60702f912ee3896099fe
Moses-93 <moieseenko2001@gmail.com>
2024-03-17 15:31:17 +0200

    task_1_add_try_except

--- task-1/task_1.py
+++ task-1/task_1.py
@@ -1,12 +1,14 @@
 def total_salary(path): # функція яка приймає шлях до файлу 
-    salaries = []  # список для зберігання зарплат
-    with open(path, "r", encoding="utf-8") as file: # відкриваємо та читаємо файл за допомогою менеджера контексту  
-        for read in file.readlines():
-            _, salary = read.split(",")
-            salaries.append(int(salary))  # додаємо зарплату до списку
-
-    full = sum(salaries)  # сумуємо ЗП 
-    medium = full / len(salaries)  # ділимо на кількість робітників 
+    try:
+        salaries = []  # список для зберігання зарплат
+        with open(path, "r", encoding="utf-8") as file: # відкриваємо та читаємо файл за допомогою менеджера контексту  
+            for read in file.readlines():
+                _, salary = read.split(",")
+                salaries.append(int(salary))  # додаємо зарплату до списку
+        full = sum(salaries)  # сумуємо ЗП 
+        medium = full / len(salaries)  # ділимо на кількість робітників 
+    except ValueError as error:
+        print(error)
 
     return "Загальна сумма:", full, "Cередня сумма:", medium # повертаємо загальну та середню зп 
 
